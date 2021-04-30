dd550a4a700623b17f2a15b2a5057f880ed369a9
unknown <ccotton@mail.fresnostate.edu>
2021-04-29 23:21:58 -0700

    finished structured english

--- Exam2.py
+++ Exam2.py
@@ -1,32 +1,25 @@
-# Code for sorting grades
+"""Plain English
 
-def calculate_percentage_above_average(avg, grades):
-    counter = 0
-    percent = 0.0
-    for i in grades:
-        if float(i) > avg:
-            counter = counter+1
-    percent = counter/len(grades) * 100
-    return round(percent,2)
+start 
+define function to calculate what percentage of grades lie above the average
+    initialize counter and percentage 
+    create a loop that iterates through the Final.txt file  
+    add 1 to counter that tracks how many grades exist above the average value of the grades
+    divide the count of grades above the average by the total number of grades and multiply by 100
+    after this number is found, round the number to two decimal places to return the final answer
 
-def average_grade(grades):
-    add = sum(grades)
-    length = len(grades)
-    avg = add / length
-    return avg
+define a function that calculates the average grade 
+    add all of the numbers in the file up and divide by how many grades exist in the file
 
-def main():
-    a_file = open("/Users/CCott_000/Desktop/Spring2021/Test2/Final.txt", "r")
-    grades = []
-    for i in a_file:
-        grades.append(int(i))
-    avg = average_grade(grades)
-    print("Number of grades:", len(grades))
-    print("Average grade:", avg)
-    print("Percentage of grades above average: ", calculate_percentage_above_average(avg, grades), "%", sep='')
-    
-    ##print(a_file.read())
-    a_file.close()
+define main and open the text file to read 
+append all of the grades in the file into a list 
+call the function that calculates the average grade to determine the avgerage grade
 
-if __name__ == "__main__":
-    main()
+print out values that state the number of grades total, the average grade, and what percentage of grades
+lie above the average value
+use sep to remove the space between the number and the percentage sign 
+
+call main and execute all of the defined functions in the proper order and print to the user 
+the total number of grades, the average grade, and the percentage of grades above average 
+end
+"""
